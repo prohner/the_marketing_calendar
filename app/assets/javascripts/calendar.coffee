f91a3613810a19de96ae1ff8fb895269593db731
Preston Rohner <prestonrohner@me.com>
2016-03-21 21:57:52 -0700

    Added some colors to the calendar

--- app/assets/javascripts/calendar.coffee
+++ app/assets/javascripts/calendar.coffee
@@ -36,8 +36,20 @@ ready = ->
       ]
 
       eventRender: (event, element) ->
-        console.log "Checking '" + event.title + "'" if debugging
-        $(element).addClass(event.cssClass)
+        # console.log "Checking '" + event.title + "'" if debugging
+        if event.cssRules != undefined
+          console.log 'Event type ' + event.type + ' Rules: ' + event.cssRules
+          $('<style>.' + event.cssClass + ' {' + event.cssRules + '}</style>').appendTo('head');
+        $(element).addClass event.cssClass
+
+        if event.type == 'tactic'
+          $(element).css "font-style", "italic"
+
+        if event.type == 'event'
+          $(element).css "font-weight", "bold"
+          $(element).css "font-style", "italic"
+          event.rendering = 'background'
+
         # if hidden_category_groups.indexOf(event.category_group_id) >= 0
         #   # console.log "  Event '" + event.title + "' category is hidden" if debugging
         #   $(element).hide();
--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -3,6 +3,18 @@ class CalendarController < ApplicationController
   end
 
   def events
+    colors = [["PaleGoldenrod", "Black", "OrangeRed"],
+      ["MistyRose", "Black", "Red"],
+      ["LightCyan", "Black", "Green"],
+      ["PapayaWhip", "Black", "Salmon"],
+      ["PowderBlue", "Black", "RoyalBlue"],
+      ["Tan", "Black", "Teal"],
+      ["Linen", "Black", "PowderBlue"],
+      ["HoneyDew", "Black", "Gainsboro"],
+      ["CornSilk", "Black", "Coral"],
+      ["AliceBlue", "Black", "Aqua"],
+    ]
+
     events = Event.all.map do |event|
       {
         :id => "event-#{event.id}",
@@ -20,50 +32,63 @@ class CalendarController < ApplicationController
     end
 
     tactics = Tactic.all.map do |tactic|
+      bg = colors[tactic.id % colors.length][0]
+      fg = colors[tactic.id % colors.length][1]
+      border = colors[tactic.id % colors.length][2]
+
+      c = "background-color:#{bg} !important; "\
+          "border-color:lightgray !important; "\
+          "border-left-color:#{border} !important; "\
+          "border-left-width:thick !important; "\
+          "color:#{fg};"
+
       {
         :id => "tactic-#{tactic.id}",
-        :title => tactic.title,
+        :title => "#{tactic.title}",
         :description => tactic.title,
         :start => tactic.starts_on.noon,
         :realStart => tactic.starts_on.noon,
         :alternateStart => (tactic.ends_on - 1.day),
         :end => tactic.ends_on,
-        :color => "rgb(147,215,147)",
+        :color => "RoyalBlue",
         :borderColor => "#d1efc4",
-        :textColor => "#3f3f3f",
+        :textColor => fg,
         :allDay => true,
-        :type => :tactic
+        :type => :tactic,
+        :cssClass => "tactic-#{tactic.id}",
+        :cssRules => c
       }
     end
 
-    tactic = Tactic.first
-    tactics =
-      [{
-        :id => "tactic-#{tactic.id}",
-        :title => tactic.title,
-        :description => tactic.title,
-        :start => tactic.starts_on.noon,
-        :realStart => tactic.starts_on.noon,
-        :alternateStart => (tactic.ends_on - 1.day),
-        :end => tactic.ends_on,
-        :color => "rgb(147,215,147)",
-        :borderColor => "#d1efc4",
-        :textColor => "#3f3f3f",
-        :allDay => true,
-        :type => :tactic,
-        :cssClass => "tactic-#{tactic.id}"
-      }]
+    # tactic = Tactic.first
+    # tactics =
+    #   [{
+    #     :id => "tactic-#{tactic.id}",
+    #     :title => tactic.title,
+    #     :description => tactic.title,
+    #     :start => tactic.starts_on.noon,
+    #     :realStart => tactic.starts_on.noon,
+    #     :alternateStart => (tactic.ends_on - 1.day),
+    #     :end => tactic.ends_on,
+    #     # :color => "rgb(147,215,147)",
+    #     # :borderColor => "#d1efc4",
+    #     # :textColor => "#3f3f3f",
+    #     :allDay => true,
+    #     :type => :tactic,
+    #     :cssClass => "tactic-#{tactic.id}",
+    #     :cssRules => "border-left-color:#{colors[tactic.id % colors.length]}; border-left-width:thick; color:black;"
+    #   }]
 
-    steps = tactic.steps.all.map do |step|
+    steps = Step.all.map do |step|
       {
         :id => "step-#{step.id}",
-        :title => step.title,
+        :title => "#{step.title}",
         :description => step.tactic.title + ' - ' + step.title,
         :start => step.starts_on,
         :end => step.ends_on,
-        :color => "#dddddd",
-        :borderColor => "#cdcdcd",
-        :textColor => "#3f3f3f",
+        # :color => "#dddddd",
+        # :borderColor => "#cdcdcd",
+        # :textColor => "#3f3f3f",
         :allDay => true,
         :type => :step,
         :cssClass => "tactic-#{step.tactic.id}"
