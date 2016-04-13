e0d022b84736609d1314a855e5aef7e7d0a0487e
Preston Rohner <prestonrohner@me.com>
2016-04-12 23:10:15 -0700

    Add controllers to support entry on front-end

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get 'calendar/events'
   get 'calendar/channels'
 
+  get 'tactics/new' => 'tactics#new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
